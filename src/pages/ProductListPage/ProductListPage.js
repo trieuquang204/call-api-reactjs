da77227411c33c197edcac4c6e83301389025425
trieuquang <trieuquang071997@gmail.com>
2020-12-24 16:07:19 +0700

    93 tach axios

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
 import ProductList from "../../components/ProductList/ProductList";
 import ProductItem from "../../components/ProductItem/ProductItem";
+import axios from "axios";
+import callApi from '../../utils/apiCaller';
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class ProductListPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    callApi('products', 'GET', null).then(res => {
+      this.setState({
+        products: res.data
+      })
+    })
+  }
+
   showProducts(products) {
     var result = null;
     if (products.length > 0) {
@@ -16,7 +33,19 @@ class ProductListPage extends Component {
   }
 
   render() {
-    var {products} = this.props;
+    // var {products} = this.props;
+    var products = this.state.products;
+    axios({
+      method: "GET",
+      url: "http://localhost:3000/products",
+    })
+      .then((res) => {
+        products = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     return (
       <div className="col-md-12">
         <button className="btn btn-info mt-3 mb-3">Them san pham</button>
@@ -28,8 +57,8 @@ class ProductListPage extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    products : state.products
-  }
-}
+    products: state.products,
+  };
+};
 
 export default connect(mapStateToProps, null)(ProductListPage);
