da5c4625e24c9db6afb270ffdcdcc80bac27a5f2
trieuquang <trieuquang071997@gmail.com>
2021-06-15 09:42:45 +0700

    88 Integration react router part 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Menu from "./components/Menu/Menu";
 import ProductList from "./components/ProductList/ProductList";
+import routes from "./routes";
 class App extends Component {
+  showContentMenus = (routes) => {
+    let result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        );
+      });
+    }
+    return <Switch>{result}</Switch>;
+  };
+
   render() {
     return (
-      <>
-        <Menu />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <button className="btn btn-info mt-3 mb-3">Them san pham</button>
-              <ProductList />
-            </div>
+      <Router>
+        <div>
+          <Menu />
+          <div className="container">
+            <div className="row">{this.showContentMenus(routes)}</div>
           </div>
         </div>
-      </>
+      </Router>
     );
   }
 }
